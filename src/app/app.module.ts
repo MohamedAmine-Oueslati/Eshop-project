33ab6352ed26e55c3ad327b7c9f022f75f0ab44e
MohamedAmine-Oueslati <wesamin1991@gmail.com>
2020-10-15 21:24:53 +0100

    project init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+import { NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ProductsComponent } from './products/products.component';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    LoginComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    RouterModule.forRoot([
+      {path: '' , component: HomeComponent },
+      {path: 'Login' , component: LoginComponent },
+      {path: 'Products' , component: ProductsComponent }
+
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCv7IlZcFTicy-jd_T-wpehKIalIWWwKE8",
+    authDomain: "eshop-project-1553f.firebaseapp.com",
+    databaseURL: "https://eshop-project-1553f.firebaseio.com",
+    projectId: "eshop-project-1553f",
+    storageBucket: "eshop-project-1553f.appspot.com",
+    messagingSenderId: "252268771797",
+    appId: "1:252268771797:web:bfd6d0a9a5755bb0b4a55c",
+    measurementId: "G-JD89779W8P"
+  }
 };
