c82fc8236862524ae25c50282ef60bca7d880143
MohamedAmine-Oueslati <wesamin1991@gmail.com>
2020-10-21 21:47:28 +0100

    store data firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from './auth.service';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
@@ -9,9 +10,10 @@ import { AuthService } from './auth.service';
 })
 export class AppComponent {
   title = 'Eshop-project';
-  constructor(private auth: AuthService, router:Router){
+  constructor(private userService: UserService ,private auth: AuthService, router:Router){
     auth.user$.subscribe(user => {
       if (user) {
+        this.userService.save(user)
         let returnUrl = localStorage.getItem('returnUrl')
         router.navigateByUrl(returnUrl)
       }
