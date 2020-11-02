5670c6e444eeaa96072a949cbbc00141a3c2ec71
MohamedAmine-Oueslati <wesamin1991@gmail.com>
2020-11-02 17:36:24 +0100

    products component done

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CategoryService } from '../services/category.service';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-products',
@@ -6,8 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
+  products: any = [];
+  categories: any;
+  category:any;
+  filtered: any = [];
 
-  constructor() { }
+  constructor(
+    private productService:ProductService,
+    private categoryService:CategoryService,
+    private route: ActivatedRoute
+    ) {
+    this.productService.products.subscribe(res => this.filtered = this.products = res)
+    this.categoryService.getCategories().subscribe(res => this.categories = res)
+
+    this.route.queryParamMap.subscribe(data => {
+      this.category = data.get('category')
+      console.log(this.category)
+      this.filtered = (this.category) ? 
+      this.products.filter((p: { data: { category: any; }; }) => p.data.category === this.category) :
+      this.products
+    })
+   }
 
   ngOnInit(): void {
   }
